1072698b26f9c1b1f4770a5a062d661c2a98c46b
saberda <630991493@qq.com>
2020-03-04 12:28:00 -0500

    add a empty judge

--- C++/122-bestTimeToBuyAndSellStockII.cpp
+++ C++/122-bestTimeToBuyAndSellStockII.cpp
@@ -7,6 +7,10 @@ int main() {
 
     vector<int> prices = { 1,2,3,4,5 };
 
+    if (prices.empty()) {
+        return 0;
+    }
+
     int profit = 0;
     for (int i = 1; i < prices.size(); i++) {
         if (prices[i] > prices[i-1]) {
