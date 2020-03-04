4e4bfc06efa16a0aba0943d61c4963acf0ea3ad1
saberda <630991493@qq.com>
2020-03-04 12:15:27 -0500

    add 122

---
+++ C++/122-bestTimeToBuyAndSellStockII.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector<int> prices = { 1,2,3,4,5 };
+
+    int profit = 0;
+    for (int i = 1; i < prices.size(); i++) {
+        if (prices[i] > prices[i-1]) {
+            profit += prices[i] - prices[i-1];
+        }
+    }
+    cout << profit << endl;
+
+    return 0;
+}
