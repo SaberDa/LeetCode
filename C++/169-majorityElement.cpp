9a27b1ac81ba8387c09914b4705742865a714b37
saberda <630991493@qq.com>
2020-03-05 16:24:49 -0500

    add 169 in Boyer-Moore Majority Vote algorithm

--- C++/169-majorityElement.cpp
+++ C++/169-majorityElement.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,24 +16,62 @@ int majorityElement(vector<int>& nums) {
     return nums[nums.size() >> 1];
 }
 
+// int main() {
+
+//     vector<int> nums = { 2,2,1,1,1,2,2 };
+
+//     int res = 0;
+//     unordered_map<int, int> count;
+//     for (auto n : nums) {
+//         count[n]++;
+//     }
+//     int range = 0;
+//     for (auto iter = count.begin(); iter != count.end(); iter++) {
+//         if (iter->second > range) {
+//             range = iter->second;
+//             res = iter->first;
+//         }
+//     }
+
+//     cout << res << endl;
+
+//     return 0;
+// }
+
+// solution 3
+// use Boyer-Moore Majority Vote algorithm
 int main() {
 
     vector<int> nums = { 2,2,1,1,1,2,2 };
 
-    int res = 0;
-    unordered_map<int, int> count;
-    for (auto n : nums) {
-        count[n]++;
-    }
-    int range = 0;
-    for (auto iter = count.begin(); iter != count.end(); iter++) {
-        if (iter->second > range) {
-            range = iter->second;
-            res = iter->first;
+
+    // if (nums.empty()) {
+    //     return 0;
+    // }
+
+    int count = 0;
+    int numT = 0;
+    for (auto i : nums) {
+        if (count == 0 && i != numT) {
+            numT = i;
+        }
+        if (i == numT) {
+            count++;
+        }
+        if (i != numT) {
+            count--;
         }
     }
-
-    cout << res << endl;
+    cout << numT << endl;
+    // int check = 0;
+    // for (auto i : nums) {
+    //     if (i == numT) {
+    //         check++;
+    //     }
+    // }
+    // if (check > nums.size() / 2) {
+    //     cout << numT << endl;
+    // }
 
     return 0;
 }
