50bfc4a6c9813bc80d05c2cb0232ca8b37e3b671
saberda <630991493@qq.com>
2020-03-11 17:13:03 -0400

    optimize 21

--- C++/21-mergeTwoSortedList.cpp
+++ C++/21-mergeTwoSortedList.cpp
@@ -15,33 +15,15 @@ public:
         ListNode *cur = head;
         while (l1 && l2) {
             if (l1->val <= l2->val) {
-                ListNode *temp = new ListNode(l1->val);
-                cur->next = temp;
-                cur = temp;
+                cur->next = l1;
                 l1 = l1->next;
             } else {
-                ListNode *temp = new ListNode(l2->val);
-                cur->next = temp;
-                cur = temp;
+                cur->next = l2;
                 l2 = l2->next;
             }
+            cur = cur->next;
         }
-        if (l1) {
-            while (l1) {
-                ListNode *temp = new ListNode(l1->val);
-                cur->next = temp;
-                cur = temp;
-                l1 = l1->next;
-            }
-        }
-        if (l2) {
-            while (l2) {
-                ListNode *temp = new ListNode(l2->val);
-                cur->next = temp;
-                cur = temp;
-                l2 = l2->next;
-            }
-        }
+        cur->next = l1 ? l1 : l2;
         return head->next;
     }
 }
