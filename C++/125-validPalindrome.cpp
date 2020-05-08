65ae18b8c743d8b4b87877fbbca8a60eae8f94c1
saberda <630991493@qq.com>
2020-05-08 19:58:15 -0400

    add 125

---
+++ C++/125-validPalindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string s) {
+    int i = 0;
+    int j = s.length() - 1;
+    while (i < j) {
+        while (i < j && !isalnum(s[i])) {
+            i++;
+        }
+        while (i < j && !isalnum(s[j])) {
+            j--;
+        }
+
+        if (tolower(s[i]) == tolower(s[j])) {
+            i++;
+            j--;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    cout << isPalindrome("race a car") << endl;
+    return 0;
+}
