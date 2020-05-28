cc1bd9416ab1d6eb378d076cb8470b81a0a203d2
saberda <630991493@qq.com>
2020-05-28 16:27:01 -0400

    add 117

---
+++ C++/117-populatingNextRightPointersInEachNodeII.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+Node *connect(Node *root) {
+    if (!root) {
+        return NULL;
+    }
+    deque<Node*> cur;
+    deque<Node*> next;
+    cur.push_back(root);
+    while (!cur.empty()) {
+        Node *temp = cur.front();
+        cur.pop_front();
+        temp->next = cur.empty() ? NULL : cur.front();
+        if (temp->left) {
+            next.push_back(temp->left);
+        }
+        if (temp->right) {
+            next.push_back(temp->right);
+        }
+        if (cur.empty()) {
+            swap(cur, next);
+        }
+    }
+    return root;
+}
