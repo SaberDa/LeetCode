8ef95d1483190aeaa06b8d4ab70a7e8f9b029029
saberda <630991493@qq.com>
2020-06-11 14:59:49 -0400

    add 266

---
+++ C++/266-palindromePermutation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+bool canPermutePalindrome(string s) {
+    if (s.empty()) return false;
+    unordered_map<int, int> mp;
+    int odd = 0;
+    for (auto c : s) {
+        (mp[c]++ % 2) ? odd-- : odd++;
+    }
+    return odd <= 1;
+}
