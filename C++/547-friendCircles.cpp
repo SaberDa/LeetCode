f488708c6c12ce1caa7ac1455dfc421d2c7b5c8d
saberda <630991493@qq.com>
2020-06-24 17:55:41 -0400

    add 547

---
+++ C++/547-friendCircles.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <numeric>
+
+using namespace std;
+
+int find(vector<int>& parents, int c) {
+    return (parents[c] == c) ? parents[c] : find(parents, parents[c]);
+}
+
+int findCircleNum(vector<vector<int>>& M) {
+    if (M.empty()) return 0;
+    vector<int> parents(M.size());
+    iota(parents.begin(), parents.end(), 0);
+    for (int i = 0; i < M.size(); i++) {
+        for (int j = 0; j < M[0].size(); j++) {
+            if (M[i][j]) {
+                int root1 = find(parents, i);
+                int root2 = find(parents, j);
+                parents[root1] = parents[root2];
+            }
+        }
+    }
+    unordered_set<int> st;
+    for (int i = 0; i < parents.size(); i++) st.insert(find(parents, i));
+    return st.size();
+}
