254a05b8b96c43573f3001183fd343edb88eac99
saberda <630991493@qq.com>
2020-06-26 22:43:36 -0400

    add 236

---
+++ binaryTree/236-lowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root || root == q || root == p) return root;
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+    if (!left && !right) return NULL;
+    if (left && right) return root;
+    return left == NULL ? right : left;
+}
