090c5cb977a8b18e977ddd671a47583678f10026
saberda <630991493@qq.com>
2020-06-27 21:55:18 -0400

    add 19

---
+++ linkedList/19-removeNthNodeFromEndOfList.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(nullptr) {}
+};
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode *slow = head, *fast = head;
+    while (n--) fast = fast->next;
+    if (!fast) return head->next;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    slow->next = slow->next->next;
+    return head;
+}
