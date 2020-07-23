1464676148ad7b64bf19ae62c1211c563f7b85a3
saberda <630991493@qq.com>
2020-07-23 19:50:22 -0400

    add 870

---
+++ C++/870-advantageShuffle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+vector<int> advantageCount(vector<int>& A, vector<int>& B) {
+    multiset<int> s(A.begin(), A.end());
+    for (int i = 0; i < B.size(); i++) {
+        auto p = s.upper_bound(B[i]);
+        if (p == s.end()) p = s.begin();
+        A[i] = *p;
+        s.erase(p);
+    }
+    return A;
+}   
