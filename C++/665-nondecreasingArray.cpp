80e7aa8f13d4329fa42a5744895e22e11411d5c8
saberda <630991493@qq.com>
2020-08-11 15:26:55 -0400

    add 665

---
+++ C++/665-nondecreasingArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool checkPossibility(vector<int>& nums) {
+    int count = 0;
+    for (int i = 1, j = 0; i < nums.size(); i++, j++) {
+        if (nums[j] > nums[i]) {
+            count++;
+            if (j > 0 && nums[j - 1] > nums[i]) nums[i] = nums[j];
+            else nums[j] = nums[i];
+        }
+    }
+    return count <= 1;
+}
