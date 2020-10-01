964e2c8920f541c1dfb24b9823a5f9b823410e98
saberda <630991493@qq.com>
2020-10-01 18:07:32 -0400

    add 1223

---
+++ C++/1223-diceRollSimulation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dieSimulator(int n, vector<int> &rollMax) {
+    long p = 1e9 + 7;
+    vector<vector<long>> dp(n + 1, vector<long>(6, 0));
+    vector<long> sum(n + 1, 0);
+    sum[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < 6; j++) {
+            for (int k = 1; k <= rollMax[j] && k <= i; k++) {
+                dp[i][j] = (dp[i][j] + sum[i - k] - dp[i - k][j] + p) % p;
+            }
+            sum[i] = (sum[i] + dp[i][j]) % p;
+        }
+    }
+    return sum.back();
+}
