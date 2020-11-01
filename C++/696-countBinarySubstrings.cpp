7ded7a3edfe452f3917f4271b80b02216b0338ca
saberda <630991493@qq.com>
2020-11-01 16:19:15 -0500

    add 696

---
+++ C++/696-countBinarySubstrings.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+int countBinarySubstrings(string s) {
+    int count = 0;
+    for (int i = 0, j = 0; i < s.size(); j = i) {
+        int a = 0, b = 0;
+        while (j < s.size() && s[j] == s[i]) j++, a++;
+        i = j;
+        while (j < s.size() && s[j] == s[i]) j++, b++;
+        count += min(a, b);
+    }
+    return count;
+}
