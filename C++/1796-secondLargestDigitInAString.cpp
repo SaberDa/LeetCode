0990d5062efb4d1aef840d7d1efce18041e02a91
saberda <630991493@qq.com>
2021-05-21 19:59:56 -0400

    add 1796

---
+++ C++/1796-secondLargestDigitInAString.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        int first = -1, second = -1;
+        for (auto c : s) {
+            if (isdigit(c)) {
+                int cur = c - '0';
+                if (cur > first) {
+                    second = first;
+                    first = cur;
+                } else if (cur != first && cur > second) {
+                    second = cur;
+                }
+            }
+        }
+        return second;
+    }
+};
